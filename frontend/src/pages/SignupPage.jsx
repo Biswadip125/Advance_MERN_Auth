80acf3b6d4b14a600e9ff28b9c3aa8497e04936e
Biswadip125 <biswadipchowdhury114@gmail.com>
2025-08-10 13:52:02 +0530

    chore(frontend): initialize project with signup page and password strength meter

---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,74 @@
+import { motion } from "motion/react";
+import Input from "../components/Input";
+import { Lock, Mail, User } from "lucide-react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+
+const SignupPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800/50 backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Create Account
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <Input
+            icon={User}
+            type={"text"}
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            icon={Mail}
+            type={"email"}
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type={"password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/*Password strength meter */}
+          <PasswordStrengthMeter password={password} />
+          <motion.button
+            className="mt-5 bg-gradient-to-r from-green-500 to-emerald-600 w-full px-4 py-3 rounded-lg text-white cursor-pointer font-bold shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Sign Up
+          </motion.button>
+        </form>
+      </div>
+
+      <div className="px-8 py-4 bg-gray-900/50 text-white flex items-center justify-center">
+        <p className="text-sm text-gray-400 ">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+export default SignupPage;
